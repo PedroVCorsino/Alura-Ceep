32e0f5a04a4de1967f71d563854830cba556dc1b
Pedro Vinicius <pedro.vinicius@acai.com.br>
2021-10-18 17:55:59 -0300

    filtrar datas

--- projeto_inicial/componentes/carregaTarefas.js
+++ projeto_inicial/componentes/carregaTarefas.js
@@ -1,4 +1,5 @@
-import { Tarefa } from "./criaTarefa.js"
+import { removeDatasRepetidas } from "../service/data.js"
+import { criaData } from "./criaData.js"
 
 export const carregaTarefa = () => {
     const lista = document.querySelector('[data-list]')
@@ -6,7 +7,9 @@ export const carregaTarefa = () => {
     const tarefasCadastradas = JSON.parse(localStorage.getItem('tarefas')) || []
 
     lista.innerHTML = " "
-    tarefasCadastradas.forEach((tarefa) => {
-        lista.appendChild(Tarefa(tarefa))        
+    const datasUnicas = removeDatasRepetidas(tarefasCadastradas)
+    datasUnicas.forEach((dia) => {
+       
+        lista.appendChild(criaData(dia)) 
     })
 }
